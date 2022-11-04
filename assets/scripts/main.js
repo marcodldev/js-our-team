2a0821f13ff3edb39e6fc5e8d2c38ca0a066a88b
marcodldev <marcodelisi2@gmail.com>
2022-11-04 17:36:09 +0100

    + cards

---
+++ assets/scripts/main.js
@@ -0,0 +1,58 @@
+// Viene fornito un file js in cui è definito un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// MILESTONE 1:
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// organizzare i singoli membri in card/schede
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+for (let i=0; i < team.length; i++) {
+
+    let elementoCorrente = team[i];
+
+    document.getElementById("container1").innerHTML += `
+    <div class="card" style="width: 18rem;">
+        <img src="./assets/img/${elementoCorrente.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${elementoCorrente.name}</h5>
+            <p class="card-text">${elementoCorrente.role}</p>          
+        </div>
+    </div>
+    `
+}
